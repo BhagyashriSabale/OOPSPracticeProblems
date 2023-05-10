0708552177608ee0d11e0e29ccfa31a41b510b79
BhagyashriSabale <balisabale16@gmail.com>
2023-05-10 23:13:21 +0530

    [Bhagyashri]Print Unique Number in array

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@
         {
             Console.WriteLine("Welcome OOPS Array Practice Problem");
             Console.WriteLine("Choose 1 Program from below list:");
-            Console.WriteLine("1.CountofDuplicates");
+            Console.WriteLine("1.CountofDuplicates\n2.UniqueElements");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     TotalDuplicates duplicate = new TotalDuplicates();
                     duplicate.DuplicateElement();
+                    break;
+                case 2:
+                    UniqueElements unique = new UniqueElements();
+                    unique.UniqueEle();
                     break;
             }
         }
---
+++ UniqueElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPracticePrograms
+{
+    internal class UniqueElements
+    {
+        public void UniqueEle()
+        {
+            int[] arr = { 1, 3, 2, 4, 5, 3, 4, 5, 6, 4, 5, 6 };
+            Console.Write("Unique elements in array: ");
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                bool isDuplicate = false;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    Console.Write(arr[i] + " ");
+                }   
+            }
+        }
+    }
+}
