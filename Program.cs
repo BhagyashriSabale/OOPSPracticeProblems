64eeb7575650ca27e3b7aca75a7c106e2b12cb93
BhagyashriSabale <balisabale16@gmail.com>
2023-05-11 01:34:18 +0530

    [Bhagyashri] Reverse String

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome OOPS Array Practice Problem");
             Console.WriteLine("Choose 1 Program from below list:");
-            Console.WriteLine("1.CountofDuplicates\n2.UniqueElements\n3.FindMinMax\n4.Pattern1\n5.Pattern2");
+            Console.WriteLine("1.CountofDuplicates\n2.UniqueElements\n3.FindMinMax\n4.Pattern1\n5.Pattern2\n6.ReverseString");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -29,6 +29,10 @@
                 case 5:
                     Pattern2 pattern2 = new Pattern2();
                     pattern2.StarPattern();
+                    break;
+                case 6:
+                    ReverseS reverse = new ReverseS();
+                    reverse.ReverseString();
                     break;
             }
         }
---
+++ ReverseS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPracticePrograms
+{
+    internal class ReverseS
+    {
+        public void ReverseString()
+        {
+            string inputString = "I love to play Badminton";
+            string[] words = inputString.Split(' ');
+            string reversedString = "";
+
+            foreach (string word in words)
+            {
+                char[] letters = word.ToCharArray();
+                Array.Reverse(letters);
+                reversedString += new string(letters) + " ";
+            }
+
+            Console.WriteLine("Original string: " + inputString);
+            Console.WriteLine("Reversed string: " + reversedString.Trim());
+        }
+    }
+}
