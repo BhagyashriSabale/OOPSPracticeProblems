3f4fa8427faa3361cad3f1918a5c79d1a51e257b
BhagyashriSabale <balisabale16@gmail.com>
2023-05-10 22:40:19 +0530

    [Bhagyashri] welcome msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPSPracticePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome OOPS Array Practice Problem");
+        }
+    }
+}
