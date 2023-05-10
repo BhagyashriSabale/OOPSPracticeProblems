a42c11cb5dfa7e96b891c681ef31c6f9d27aac97
BhagyashriSabale <balisabale16@gmail.com>
2023-05-10 22:59:27 +0530

    [Bhagyashri]Count of total duplicates

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome OOPS Array Practice Problem");
+            Console.WriteLine("Choose 1 Program from below list:");
+            Console.WriteLine("1.CountofDuplicates");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    TotalDuplicates duplicate = new TotalDuplicates();
+                    duplicate.DuplicateElement();
+                    break;
+            }
         }
     }
 }
---
+++ TotalDuplicates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPracticePrograms
+{
+    internal class TotalDuplicates
+    {
+        public void DuplicateElement()
+        {
+            int[] arr = { 1, 3, 2, 4, 5, 3, 4, 5, 6, 4, 5, 6 };
+            int count = 0;
+
+            for(int i=0; i<arr.Length; i++)
+            {
+                for (int j=i+1; j<arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+
+                }
+            }
+            Console.WriteLine("Total No.of Duplicates in array: " + count);
+
+        }
+    }
+}
